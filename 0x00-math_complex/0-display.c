8d99de2467fe9488e7fdf650c89d9936a65d8222
KevinAndresG <3348@holbertonschool.com>
2021-07-30 18:19:37 -0500

    fix

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,10 +8,24 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
+	if (c.im < 0)
+	{
+		printf("%.0f - %.0fi\n", c.re, c.im);
+	}
+	else if (c.re == 0)
+	{
+		printf("%.0fi\n", c.im);
+	}
+	else if (c.im == 0)
 	{
 		printf("%.0f\n", c.re);
 	}
+	else if (c.re == 0 && c.im == 0)
+	{
+		return;
+	}
+
+
 	else
 		printf("%.0f + %.0fi\n", c.re, c.im);
 }
--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 #include <stdio.h>
+#include <stdlib.h>
+
 typedef struct numbers
 {
 	double re;
