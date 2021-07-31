67aca8439483f5b89180e58f79537f3615d3e5a2
KevinAndresG <3348@holbertonschool.com>
2021-07-30 20:13:30 -0500

    fix

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,20 +10,38 @@ void display_complex_number(complex c)
 {
 	if (c.re == 0 && c.im == 0)
 	{
-		return;
 	}
-	else if (c.im < 0)
+	else if (c.re == 0 && c.im == 1)
 	{
-		printf("%.0f - %.0fi\n", c.re, c.im * (-1));
+		printf("i\n");
 	}
-	else if (c.re == 0)
+	else if (c.re == 0 && c.im == -1)
 	{
-		printf("%.0fi\n", c.im);
+		printf("-i\n");
 	}
-	else if (c.im == 0)
+
+	else if (c.re != 0 && c.im == 1)
+	{
+		printf("%.0f + i\n", c.re);
+	}
+	else if (c.re != 0 && c.im == -1)
+	{
+		printf("%.0f - i\n", c.re);
+	}
+	else if (c.re == 0 && c.im != 0)
+	{
+		printf("%.0f\n", c.im);
+	}
+	else if (c.re != 0 && c.im == 0)
 	{
 		printf("%.0f\n", c.re);
 	}
+
+	else if (c.im < 0 && c.re != 0)
+	{
+		printf("%.0f - %.0fi\n", c.re, c.im * (-1));
+	}
+
 
 	else
 		printf("%.0f + %.0fi\n", c.re, c.im);
